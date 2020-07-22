f0a906ec76126db844973a1fa17b6a6acf70ad30
3inchtime <3inchtime@gmail.com>
2020-07-23 00:04:08 +0800

    nginx fix

--- domain/internal/service/routes.go
+++ domain/internal/service/routes.go
@@ -6,9 +6,9 @@ func (s *Service) InitRoutes() {
 		httpRoutes.GET("/get_video", s.HttpGetVideoInfo)
 		httpRoutes.POST("/create_video", s.HttpCreateVideo)
 	}
-	grpcRoutes := s.Router.Group("/grpc")
+	gRPCRoutes := s.Router.Group("/grpc")
 	{
-		grpcRoutes.GET("/get_video", s.GRPCGetVideoInfo)
-		grpcRoutes.POST("/create_video", s.GRPCCreateVideo)
+		gRPCRoutes.GET("/get_video", s.GRPCGetVideoInfo)
+		gRPCRoutes.POST("/create_video", s.GRPCCreateVideo)
 	}
 }
